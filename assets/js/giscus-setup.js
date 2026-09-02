6f7198264829b1c256774ec44daf4cee64a88380
Carmin-Lab <3161258561@qq.com>
2026-09-02 16:10:08 +0000

    Deploying to gh-pages from @ CardioMind-AI/CardioMind-AILab.github.io@8b296db6426c55bdbfafcc6fb957ed6c9635f0a1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yirong-CEO/CardioMind-AILab.github.io",
+    "data-repo": "CardioMind-AI/CardioMind-AILab.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
